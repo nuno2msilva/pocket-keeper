22513cb2694d92672575164f2730b4adc4be9e91
Nuno Silva <166436702+nuno2msilva@users.noreply.github.com>
2025-12-29 00:38:51 +0000

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: "/your-repo-name/",  server: {
+base: "/pocket-keeper/",  
+server: {
     host: "::",
     port: 8080,
   },
