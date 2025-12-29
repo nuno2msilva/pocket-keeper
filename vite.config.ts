c92058e784526276dfc7ed991aa5018c076e2777
Nuno Silva <166436702+nuno2msilva@users.noreply.github.com>
2025-12-29 00:35:45 +0000

    Set base path for Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+base: "/your-repo-name/",  server: {
     host: "::",
     port: 8080,
   },
